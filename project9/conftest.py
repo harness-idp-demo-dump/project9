4657f5e82c57b8bf7cf9ebab3407d2c4f9ae19e6
OrkoHunter <>
2024-01-03 04:04:42 +0000

    Project init

---
+++ project9/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from project9.users.models import User
+from project9.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
